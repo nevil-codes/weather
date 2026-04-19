35ba05cc7ab96800fee74caebc2e1deba4bf1682
nick <pip install numpy pandas matplotlib seaborn jupyter ipykernel sqlalchemy ipython-sql>
2026-04-19 19:35:21 +0200

    chore: initialize Vite React project with clean boilerplate

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+// Render the App component into the root div
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
